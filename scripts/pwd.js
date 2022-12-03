190a63f4d1e21377dae8a921cc7fe135379d82e8
jtagrgh <jakob.koblinsky@gmail.com>
2022-12-03 17:59:42 -0500

    Init

---
+++ scripts/pwd.js
@@ -0,0 +1,14 @@
+// Write a clickable pwd string. Like a shell pwd
+// but clickable and written in javascript.
+
+current_path = window.location.pathname.split('/'); // Array of path directories
+current_path.pop() // Remove last element "index.html"
+web_root_pos = current_path.indexOf("208"); // Position in current_path array of root "208"
+right_path = current_path.slice(web_root_pos); // Path [web_root, ]
+left_path = current_path.slice(0, web_root_pos); // Path [system root, web_root)
+
+for (p in right_path) {
+    left_path.push(right_path[p]); // Add extend left_path w/ next dir
+    path_str = left_path.join("/") + "/index.html"; // Actually link to index.html
+    document.write("<a href=" + path_str + ">" + right_path[p] + "<a>/"); // Write to DOM
+}
